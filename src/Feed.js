20083a8c2165d4fa3ef461a98bdd8757ced8b954
Dung Dau <dudau0924222@gmail.com>
2022-03-31 10:05:09 -0400

    remove unnecessary import statement

--- src/Feed.js
+++ src/Feed.js
@@ -3,7 +3,6 @@ import './Feed.css';
 import CreateIcon from '@mui/icons-material/Create';
 import ImageIcon from '@mui/icons-material/Image';
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
-import EventNoteIcon from '@mui/icons-material/EventNote';
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
 import InputOption from './InputOption';
 import {useState, useEffect} from 'react';
